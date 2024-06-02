a031e3ccdf51225581c98eca612998601f8f1899
kotenkoI <iryna.herashchenko@gmail.com>
2024-06-02 15:28:19 +0300

    changes

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,27 +1,56 @@
 import './App.css'
-import Profile from '../Profile/Profile'
-import userData from '../data/userData.json'
-import FriendList from '../FriendList/FriendList'
-import friends from '../data/friends.json'
-import transactions from '../data/transaction.json'
-import TransactionHistory from '../TransactionHistory/TransactionHistory'
+import Description from '../Description/Description'
+import Options from '../Options/Options'
+import { useState, useEffect } from 'react'
+import Feedback from '../Feedback/Feedback'
+import Notification from '../Notification/Notification'
 
 function App() {
+ const [values, setValues] = useState(() => {
+    const savedValues = window.localStorage.getItem("saved-values");
+    if (savedValues !== null) {
+      return JSON.parse(savedValues); 
+    }
+    return {
+      good: 0,
+      neutral: 0,
+      bad: 0
+    };
+ });
 
+ useEffect(() => {
+    window.localStorage.setItem("saved-values", JSON.stringify(values)); 
+ }, [values]);
 
-  return (
+ const updateFeedback = (feedbackType) => {
+    setValues(prevValues => {
+      const updatedValues = {
+        ...prevValues,
+        [feedbackType]: prevValues[feedbackType] + 1
+      };
+      return updatedValues;
+    });
+ }
+
+ const resetFeedback = () => {
+    const resetValues = {
+      good: 0,
+      neutral: 0,
+      bad: 0
+    };
+    setValues(resetValues);
+ }
+
+ const totalFeedback = Object.values(values).reduce((acc, curr) => acc + curr, 0);
+ const positiveFeedback = totalFeedback > 0 ? Math.round((values.good / totalFeedback) * 100) : 0;
+
+ return (
     <>
-      <Profile
-        name={userData.username}
-        tag={userData.tag}
-        location={userData.location}
-        image={userData.avatar}
-        stats={userData.stats}
-      />
-      <FriendList friends={friends} />
-      <TransactionHistory transactions={transactions} />
+      <Description />
+      <Options setValue={updateFeedback} resetValues={resetFeedback} feedbackcount={totalFeedback} />
+      {totalFeedback > 0 ? <Feedback value={values} totalfeedback={totalFeedback} positivefeedback={positiveFeedback} /> : <Notification />} 
     </>
-  )
+ )
 }
 
 export default App
